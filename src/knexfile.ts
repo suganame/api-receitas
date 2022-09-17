49818d9e8d6357b23b05d7be1cfb739904ce2a3d
Guilherme Suganame <gsuganame@gmail.com>
2022-09-17 02:26:41 -0300

    refactor: remove useless code

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -17,38 +17,6 @@ const config: { [key: string]: Knex.Config } = {
     },
     useNullAsDefault: true,
   },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
 };
 
 module.exports = config;
